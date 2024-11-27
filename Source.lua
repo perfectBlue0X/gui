67cd913f2a74446281353d117c27fa9f56853cb3
riruru111 <greenskyisbad@gmail.com>
2024-11-27 18:51:20 +0600

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -204,7 +204,7 @@ function Luxt1.CreateWindow(libName, logoId)
     wave.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     wave.BackgroundTransparency = 1.000
     wave.Position = UDim2.new(0.0213434305, 0, 0, 0)
-    wave.Size = UDim2.new(0.157865659, 0, 0.557522118, 0)
+    wave.Size = UDim2.new(1.2, 0, 0.557522118, 0)
     wave.Image = "https://assetdelivery.roblox.com/v1/asset/?id=112579771031225"
     wave.ImageColor3 = Color3.fromRGB(181, 249, 255)
     wave.ImageTransparency = 0.300
